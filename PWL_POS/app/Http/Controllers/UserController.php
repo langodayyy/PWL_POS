e08b9cf7bc7c85de8104f870ec224b0867e9a0b6
Lucky Kurniawan Langoday <luckykrnwn17@gmail.com>
2024-03-08 07:39:29 +0700

    Praktikum 1 - $fillable:

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -10,27 +10,13 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        // $user = UserModel::all();
-        // return view('user',['data'=>$user]);
-
-        // $data = [ //tambah data user dengan Eloquent Model
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id'=>4
-        // ];
-        // UserModel::insert($data); //tambahkan data ke tabel m_user
-
-        // //coba akses model UserModel
-        // $user = UserModel::all(); //ambil semua data dari tabel
-        // return view('user',['data'=>$user]); //tampilkan ke view user
-
-        //tambah data user dengan Eloquent Model
-        $data =[
-            'nama'=> 'Pelanggan Pertama'
+        $data = [ //tambah data user dengan Eloquent Model
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        //update data user
-        UserModel::where('username','customer-1')->update($data);
+        UserModel::create($data);
         //ambil semua data user
         $user = UserModel::all();
         //tampilkan ke view user
